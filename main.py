363618f9b22bad132115bfa4b4b02768cb2223b9
Rinaish <rinaish18@gmail.com>
2026-09-03 13:35:12 +0300

    added districts' name column validation

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 import os
+import pandas as pd
 import matplotlib.pyplot as plt
 
-from src.load_data import load_all_data
-from src.criteria import *
-from config import CRITERIA, CMAPS
 from dotenv import load_dotenv
-
 load_dotenv()
+from modules.gee_auth import ee
+from modules.load_data import load_all_data
+from modules.criteria import *
+from config import CRITERIA, CMAPS
+
+def validate_districts(df):
+
+    if not 'name' in df.columns:
+        df = df.reset_index().rename(columns={"index": "name"})
+    elif df['name'].isna().any():
+        df['name'] = df.apply(lambda r: str(r.name) if pd.isna(r['name']) else r['name'], axis=1)
+        
+    return df
+
 
 def plot_map(gdf, col, output, cmap):
     fig, ax = plt.subplots(figsize=(10, 8))
@@ -38,7 +49,6 @@ def plot_map(gdf, col, output, cmap):
     )
 
     plt.tight_layout()
-    os.makedirs(output, exist_ok=True)
     plt.savefig(output, dpi=300, bbox_inches='tight')
     plt.close()
 
@@ -83,6 +93,7 @@ def main():
     else:
         green_area, air, lst, bld, roads, districts_gdf = data.values()
 
+    districts_gdf = validate_districts(districts_gdf)
     districts_gdf['area_ha'] = districts_gdf.geometry.area / 10000
     districts_ee = geemap.gdf_to_ee(districts_gdf)
 
@@ -98,6 +109,7 @@ def main():
     normalized_gdf = normalize_criteria(districts_gdf, results)
     results['integral'] = calc_integral_idx(normalized_gdf)
 
+    os.makedirs('results/figures', exist_ok=True)
     for crit, gdf in results.items():
         save_results(crit, gdf)
         
