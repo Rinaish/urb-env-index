ba80e5af35f94aeafbe1aa033c01d579008b7557
Rinaish <rinaish18@gmail.com>
2026-09-06 18:55:57 +0300

    removed repeated authorization

--- modules/gee_auth.py
+++ modules/gee_auth.py
@@ -3,17 +3,21 @@ import ee
 from modules.load_data import get_path
 
 def  _initialize_ee():
-    print("Starting ee initialization")
-
     auth_mode = get_path('GEE_AUTH_MODE')
     project_id = get_path('PROJECT_ID')
 
     try:
-        ee.Authenticate(auth_mode=auth_mode)
         ee.Initialize(project=project_id)
+        print('EE initialization successfull')
 
-    except Exception as e:
-        print(f'Authentication error: {e}')
-        raise
+    except Exception:
+        try:
+            print('Starting EE authentification')
+            ee.Authenticate(auth_mode=auth_mode)
+            ee.Initialize(project=project_id)
+
+        except Exception as e:
+            print(f'Authentication error: {e}')
+            raise
 
 _initialize_ee()
